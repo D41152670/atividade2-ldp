57623daaa215049e2597b24f431c49d5eb3569a6
D41152670 <Thiago.Mendes@adtalem.com>
2021-03-21 19:06:12 -0300

    Change problem 2'

--- problema2/problema2.cpp
+++ problema2/problema2.cpp
@@ -6,34 +6,51 @@
 #include <cctype>
 #include <map>
 using namespace std;
+
+// Função para contabilizar a quantidade de vezes que uma letra é mencionada numa frase
 string letraFrequente( const string& text, int &mx )
 {
     map <char,int> freq;
-    for ( char c : text ) if ( isalpha( c ) ) freq[tolower(c)]++;
-    mx = 0;
-    for ( auto p : freq ) if ( p.second > mx ) mx = p.second;
-    string result;
-    for ( auto p : freq ) if ( p.second == mx ) result += p.first;
+    for ( char c : text ) 
+        if ( isalpha( c ) ) 
+            freq[tolower(c)]++;
+            mx = 0;
+
+    for ( auto p : freq ) 
+        if ( p.second > mx ) 
+            mx = p.second;
+            string result;
+    
+    for ( auto p : freq ) 
+        if ( p.second == mx ) 
+        result += p.first;
+    
     return result;
 }
-
 
 // 
 int main(){
     // irá contabilizar a quantidade de letras de uma frase
     int resultString;
     // Irá armazenar cada palavra convertendo em string (simples de capturar seu tamanho)
-    string groupString;
-    char * pch, str[] = "Uma frase fixa pq a outra não rolou";
+    string groupString, test;
+    // 
+    char * pch, str[60];
+    // 
+    cout << "Digite uma frase \n";
+    // Recebe a string de forma completa
+    getline(cin, test);
+    // converte uma string para char
+    strcpy(str, test.c_str());
     // Realiza um split da string digita quebrando a em varias palavras isoladas
-    pch = strtok (str," ,.-");
+    pch = strtok ( str, " ,.-" );
     // Laço de repetição que irá quebrando a frase em palavras
     while (pch != NULL)
     {
         // string recebe o valor quebrado do pch
         groupString = pch;
         // printa a quebra da frase
-        printf("%s\n", pch);
+        printf(" %s\n", pch);
         // irá adicionar null ao PCH quando não houver mais palavras
         pch = strtok (NULL, " ,.-");
         // realiza o somatorio de cada tamanho de palavra capturando o total da frase
@@ -41,10 +58,9 @@ int main(){
     }
     // printa o total de letras na frase
     printf("A frase contem %d letras \n\n", resultString);
-
-    string test = "Uma frase fixa pq a outra não rolou";
+    // irá armazenar o número de vezes que a letra apareceu
     int mx;
+    // inicia a variavel modes recebendo o resultado da função letra frequente
     string modes = letraFrequente( test, mx );
-    cout << test << '\n';
-    cout << "A letra mais comum foi: " << modes << "( " << mx << " vezes )\n";
+    cout << "A letra mais comum foi: " << modes << " ( " << mx << " vezes ) \n\n";
 }
--- problema2/tempCodeRunnerFile.cpp
+++ problema2/tempCodeRunnerFile.cpp
@@ -1,2 +1,22 @@
-cout << "Digite uma frase \n";    
-    // cin >> str[30];
+strcpy(str, test.c_str());
+    // Realiza um split da string digita quebrando a em varias palavras isoladas
+    pch = strtok ( str, " ,.-" );
+    // Laço de repetição que irá quebrando a frase em palavras
+    while (pch != NULL)
+    {
+        // string recebe o valor quebrado do pch
+        groupString = pch;
+        // printa a quebra da frase
+        // printf("%s\n", pch);
+        // irá adicionar null ao PCH quando não houver mais palavras
+        pch = strtok (NULL, " ,.-");
+        // realiza o somatorio de cada tamanho de palavra capturando o total da frase
+        resultString = resultString + groupString.length();
+    }
+    // printa o total de letras na frase
+    printf("A frase contem %d letras \n\n", resultString);
+    // irá armazenar o número de vezes que a letra apareceu
+    int mx;
+    // inicia a variavel modes recebendo o resultado da função letra frequente
+    string modes = letraFrequente( test, mx );
+    cout << "A letra mais comum foi: " << modes << " ( " << mx << " vezes ) \n\n";
